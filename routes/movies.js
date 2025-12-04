c408e0e95e2894200b5b3903da5b7364aaeae9d8
Gui-Pires <guipires970@gmail.com>
2025-12-04 17:42:53 -0300

    Update: New release

--- routes/movies.js
+++ routes/movies.js
@@ -12,7 +12,7 @@ router.post("/", async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
-});
+})
 
 router.get("/", async (req, res) => {
     try {
